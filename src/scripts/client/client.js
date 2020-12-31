e4639baeda6886464225369ea0a42ebf7e250a1e
Peter Smith <poida.smith@gmail.com>
2020-12-31 15:57:13 +0000

    more updates

--- src/scripts/client/client.js
+++ src/scripts/client/client.js
@@ -31,31 +31,6 @@ system.initialize = function() {
     this.registerEventData("EnchantedSign:Player", {});
 };
 
-system.log = function (...items) {
-    const toString = item => {
-        switch (Object.prototype.toString.call(item)) {
-            case '[object Undefined]':
-                return 'undefined';
-            case '[object Null]':
-                return 'null';
-            case '[object String]':
-                return `"${item}"`;
-            case '[object Array]':
-                const array = item.map(toString);
-                return `[${array.join(', ')}]`;
-            case '[object Object]':
-                const object = Object.keys(item).map(key => `${key}: ${toString(item[key])}`);
-                return `{${object.join(', ')}}`;
-            case '[object Function]':
-                return item.toString();
-            default:
-                return item;
-        }
-    }
-
-    this.emit('minecraft:display_chat_event', { message: items.map(toString).join(' ') });
-};
-
 system.emit = function(type, data) {
     let ed = this.createEventData(type);
     ed.data = data;
@@ -63,7 +38,6 @@ system.emit = function(type, data) {
 };
 
 system.onNewPlayer = function(ed) {
-    this.log("Player entered!");
     this.emit("EnchantedSign:Player", {player:ed.data.player});
 };
 
